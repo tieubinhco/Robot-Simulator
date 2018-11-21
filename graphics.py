afb3e3934143f94ca6fa25fc02cfd2453aa04bfe
niwhsa9 <ashwingupta2000@gmail.com>
2018-11-21 15:14:49 -0800

    added graphics window

--- graphics.py
+++ graphics.py
@@ -1,4 +1,11 @@
-
+import pygame
 class Graphics:
     def __init__(self, robots):
-        
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 400))
+    def translateCoord(self, x, y):
+        return
+    def updateGraphics(self):
+        pygame.event.get()
+        pygame.display.flip()
+        return
--- simulation.py
+++ simulation.py
@@ -1,14 +1,14 @@
 import threading
 import robot
 import time
-#import graphics
+import graphics
 
 #start world thread, control thread, and graphics thread 
 
 class Simulation:
     #give world,control,and graphics frequencies in hertz, give simulation rate multiple
     #allow max on world frequency to use CPU inhibited speed
-    def __init__(self, worldFreq, controlFreq, graphicsFreq, simRate, robots):
+    def __init__(self, worldFreq, controlFreq, graphicsFreq, simRate, robots, controllers):
         self.worldFreq = worldFreq
         if(self.worldFreq != 'max'):
             self.worldFreq = (1.0/worldFreq)
@@ -21,6 +21,10 @@ class Simulation:
         self.deltaT = 0
         self.prevT = time.time()
         self.robots = robots
+        self.controllers = controllers
+
+        self.speedL = 0.0
+        self.speedR = 0.0
         
         self.world = threading.Thread(target=self.worldThread)
         self.graphics = threading.Thread(target=self.graphicsThread)
@@ -28,31 +32,29 @@ class Simulation:
         self.world.start()
         self.graphics.start()
         self.control.start()
-        
 
-        
     def worldThread(self):
         self.start = time.time()
         cnt = 12 
         while(1):
             t = time.time()
-            self.robots[0].update((t - self.prevT)*self.simRate, 1.0, 1.0, 1.0)
+            self.robots[0].update((t - self.prevT)*self.simRate, 1.0, self.speedL, self.speedR)
             time.sleep(float(self.worldFreq)/float(self.simRate))
-            print(str(cnt) + " | " + str((time.time()-self.start)*self.simRate) +" | " +str(self.robots[0].getPos()[0]))
-            cnt+=1
+            #print(str(cnt) + " | " + str((time.time()-self.start)*self.simRate) +" | " +str(self.robots[0].getPos()[1]))
+            #cnt+=1
             self.prevT = t
     def controlThread(self):
-        while(1):           
+        while(1):
+            self.speedL = 1.0
+            self.speedR = 1.0
             time.sleep(self.controlFreq/self.simRate)
     def graphicsThread(self):
+        window = graphics.Graphics(self.robots)
         while(1):
+            window.updateGraphics()
             time.sleep(self.graphicsFreq/self.simRate)
 
-
-
-s = Simulation(100, 40, 30, 0.2, [ robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0) ])
-#r = robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0) 
-#for i in range(12, 250):
-#    r.update(0.01, 1.0, 1.0, 1.0)
-#    print(str(i) + " | " +str(r.getPos()[0]))
-#    print("-----")
+#battery1 = battery.Battery(1.0, 's') paramterize robot with a battery
+robot1 = robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0)
+#controller1 = pid.PID()
+s = Simulation(100, 40, 30, 1.0, [robot1], [])
