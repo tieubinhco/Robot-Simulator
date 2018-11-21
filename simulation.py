308152c742994ae9527503b71181ae5bc01330f2
niwhsa9 <ashwingupta2000@gmail.com>
2018-11-21 12:06:05 -0800

    fully selectable clock speeds and simulation rate

--- simulation.py
+++ simulation.py
@@ -19,6 +19,7 @@ class Simulation:
         self.simRate = simRate
 
         self.deltaT = 0
+        self.prevT = time.time()
         self.robots = robots
         
         self.world = threading.Thread(target=self.worldThread)
@@ -27,36 +28,29 @@ class Simulation:
         self.world.start()
         self.graphics.start()
         self.control.start()
-        #self.worldThread()
         
 
         
     def worldThread(self):
         self.start = time.time()
-        #for i in range(12, 250):
-        #    self.robots[0].update(0.01, 1.0, 1.0, 1.0)
-        #    print(str(i) + " | " + str(time.time()-self.start) +" | " +str(self.robots[0].getPos()[0]))
-        #    print("-----")
         cnt = 12 
         while(1):
-            #print(self.worldFreq)
-            self.robots[0].update(self.worldFreq, 1.0, 1.0, 1.0)
+            t = time.time()
+            self.robots[0].update((t - self.prevT)*self.simRate, 1.0, 1.0, 1.0)
             time.sleep(float(self.worldFreq)/float(self.simRate))
             print(str(cnt) + " | " + str((time.time()-self.start)*self.simRate) +" | " +str(self.robots[0].getPos()[0]))
             cnt+=1
-            
+            self.prevT = t
     def controlThread(self):
-        while(1):
-            print('alive')
+        while(1):           
             time.sleep(self.controlFreq/self.simRate)
     def graphicsThread(self):
         while(1):
-            print('alive2')
             time.sleep(self.graphicsFreq/self.simRate)
 
 
 
-s = Simulation(100, 40, 30, 0.1, [ robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0) ])
+s = Simulation(100, 40, 30, 0.2, [ robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0) ])
 #r = robot.Robot(0, 0, 0, 1.00, 0.1, 0.2286, 6.8, 0.1016, 1.67, 100, 0.0, 0.0, 0.0, 0.0) 
 #for i in range(12, 250):
 #    r.update(0.01, 1.0, 1.0, 1.0)
