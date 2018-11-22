d9a2770c8f411085faca4bc0e2a2c53689b84c62
niwhsa9 <ashwingupta2000@gmail.com>
2018-11-21 17:41:17 -0800

    wasted a bunch of time to figure out pygame

--- graphics.py
+++ graphics.py
@@ -1,11 +1,32 @@
 import pygame
-class Graphics:
+class Graphics: #graph trail, hold trail buffer array
     def __init__(self, robots):
         pygame.init()
-        self.screen = pygame.display.set_mode((1000, 400))
+        self.screen = pygame.display.set_mode((400, 400))
+        
+        self.sprite = pygame.Surface((50, 100))
+        self.sprite.fill((255,0,0))
+        self.sprite.set_colorkey((0,0,0))
+        self.rect = self.sprite.get_rect()
+        self.rect.center = (100, 100)
+
+        self.sprite, self.rect = self.rot_center(self.sprite, self.rect, 30)
+        #self.angle+=0.01
+        
     def translateCoord(self, x, y):
         return
+    
+    def rot_center(self, image, rect, angle): #Taken from pygame library
+        """rotate an image while keeping its center"""
+        rot_image = pygame.transform.rotate(image, angle)
+        rot_rect = rot_image.get_rect(center=rect.center)
+        return rot_image,rot_rect
+    
     def updateGraphics(self):
         pygame.event.get()
+        self.screen.fill((0,0,0))  
+       
+        
+        self.screen.blit(self.sprite, self.rect)
         pygame.display.flip()
         return
